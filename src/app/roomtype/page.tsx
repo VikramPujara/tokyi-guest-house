962de87bc4d3685a85b5590b802c303a631536dd
Bikram Pujara <mr.pujarabikram@gmail.com>
2025-07-21 22:48:28 +0530

    lazyloading client side build issue fixed

--- src/app/roomtype/page.tsx
+++ src/app/roomtype/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import RoomType from "../components/RoomType";
 
 const RoomPage = () => {
   return (
     <div>
-      <RoomType />
+      <Suspense fallback={<div>Loading room types...</div>}>
+        <RoomType />
+      </Suspense>
     </div>
   );
 };
